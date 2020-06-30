0cd4148444267d7071ca8d7e3390bb51e5543dcc
marcio lira <machollanda@gmail.com>
2020-06-30 17:18:36 -0300

    criando componentes com atributos interdependentes

--- src/app/courses/course-List.component.ts
+++ src/app/courses/course-List.component.ts
@@ -4,7 +4,7 @@ import { Course } from './course';
 
 @Component({
     selector: 'app-course-list',
-    templateUrl: './course-List.component.html'
+    templateUrl: 'course-list.component.html'
     //template: '<h2> Course list</h2>'
 })
 
@@ -14,10 +14,9 @@ export class CourseListComponent implements OnInit {
     ngOnInit(): void{
         this.courses = [
         {
-            
             id: 1,
             name: 'Angular: Forms',
-            imageUrl: ' ',
+            imageUrl: '/assets/images/forms.png',
             price: 99.99,
             code: 'JXG-9865',
             duration: 120,
@@ -27,7 +26,7 @@ export class CourseListComponent implements OnInit {
         {
             id: 2,
             name: 'Angular: Forms',
-            imageUrl: ' ',
+            imageUrl: '/assets/images/http.png',
             price: 99.99,
             code: 'JOK-6869',
             duration: 120,
